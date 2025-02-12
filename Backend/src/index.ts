0855cb9908e72f6321ba4dfd7c5b00ba141aa68a
ganeshburri <ganeshburri1@gmail.com>
2025-02-12 10:00:15 +0530

    fix: routes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,7 +4,7 @@ import { blogRouter } from './routes/blog';
 
 const app = new Hono();
 
-app.route('/api/v1/user', userRouter);
-app.route('/api/v1/blog', blogRouter);
+app.route('/api/v1/users', userRouter);
+app.route('/api/v1/blogs', blogRouter);
 
 export default app;
