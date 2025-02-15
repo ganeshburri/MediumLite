3f18e870b0078a184e9fe830d956e89e0d01b1e5
ganeshburri <ganeshburri1@gmail.com>
2025-02-15 15:04:54 +0530

    chore: add cors

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono';
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
-
+app.use('/*', cors());
 app.route('/api/v1/users', userRouter);
 app.route('/api/v1/blogs', blogRouter);
 
