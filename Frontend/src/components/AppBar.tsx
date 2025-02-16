a7fb05720019d0160194745dfd1b4dd1dc3071f4
ganeshburri <ganeshburri1@gmail.com>
2025-02-16 15:55:13 +0530

    feat: add blog publish page

--- Frontend/src/components/AppBar.tsx
+++ Frontend/src/components/AppBar.tsx
@@ -8,6 +8,12 @@ export const AppBar = () => {
                 Medium
             </Link>
             <div>
+                <Link to="/publish" >
+                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800
+                focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full
+                text-sm px-5 py-2.5 text-center me-2 mb-2"
+                >Write</button>
+                </Link>
                 <Avatar size="big" name={"Ganesh"} />
             </div>
         </div>
